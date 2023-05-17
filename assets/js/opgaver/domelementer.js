f7e516d426cd502fe096f4fd4f7f6c53745178a0
GBM1986 <gunnar_bjarni@hotmail.com>
2023-05-17 08:26:36 +0200

    jeg er færdig med næsten alt

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -7,7 +7,16 @@ Du skal finde elementet med id="redFigure" og console logge det.
 Du skal finde elementet med id="yellowFigure" og console logge det. 
 */
 
+
 // din kode her
+
+
+document.getElementById("redFigure");
+console.log("redFigure");
+
+document.getElementById('yellowFigure');
+console.log('yellowFigure');
+
 
 
 /* opgave 1.2
@@ -17,6 +26,9 @@ farven på elementerne skal være blå. brug evt. elelement.style.backgroundColo
 
 
 // din kode her
+
+document.getElementById('redFigure').style.backgroundColor = 'blue';
+document.getElementById('yellowFigure').style.backgroundColor = 'blue';
 
 
 
@@ -30,6 +42,18 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 // din kode her
 
 
+const headline = document.createElement("h2");
+headline.innerText = "Opgave 2.1 løsning";
+
+document.getElementById('opgaveTwo').appendChild(headline);
+
+const para = document.createElement("p");
+para.innerText = "Jeg har løst opgave 2.1";
+
+document.getElementById('opgaveTwo').appendChild(para);
+
+
+
 // opgave 3 liveHTML lists
 /* opgave 3.1
 Du skal finde alle elementer med klassen purpleFigures og console logge resultatet
@@ -37,6 +61,9 @@ Du skal finde alle elementer med klassen purpleFigures og console logge resultat
 
 // din kode her
 
+
+let alleParagraffer = document.getElementsByClassName("purpleFigures");
+console.log(alleParagraffer);
 
 /* opgave 3.2
 Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
@@ -46,6 +73,14 @@ array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScr
 
 // din kode her
 
+const purpleFigures = document.getElementsByClassName('purpleFigures');
+
+const purpleFiguresArray = Array.from(purpleFigures);
+
+purpleFiguresArray.map((figure) => {
+    figure.style.backgroundColor = 'red';
+});
+
 
 /* opgave 3.3
 Brug myLiveList til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
@@ -54,6 +89,12 @@ myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 
 
 // din kode her
+
+
+
+purpleFiguresArray.map((figure) => {
+    figure.children[0].innerHTML = 'RED';
+});
 
 
 
